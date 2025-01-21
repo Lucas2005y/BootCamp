ed805a9f755246a6ec91c6910561fc146db6364c
Lucas2005y <lucas.hs.oliveira@gmail.com>
2025-01-21 16:34:09 -0300

    CommitVScode

---
+++ build.js
@@ -0,0 +1,22 @@
+
+// async function buildProject() {
+//     try {
+//         console.log('Iniciando a construção do projeto...');
+//         someAsyncFunction();
+//         console.log('Construção concluída com sucesso.');
+//       } catch (error) {
+//         console.error('Erro durante a construção do projeto:', error);
+//       }
+//   }
+  
+// async function someAsyncFunction() {
+//     // Exemplo de função assíncrona
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve('Função assíncrona concluída.');
+//       }, 1000);
+//     });
+//   }
+  
+// buildProject();
+  
